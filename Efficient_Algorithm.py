928ca25ce2eac9d5657040606d5bc919a1c42ad8
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-01-06 14:19:23 +0800

    add: 2 questions answers

---
+++ Efficient_Algorithm.py
@@ -0,0 +1,40 @@
+# NAME: GANGULA KARTHIK 
+# ADMISSION_NO: 223715Y
+# TUTORIAL_GROUP: BA2202
+############################################################################################################
+# 2 pointer method
+# Using a while loop is much faster and more memory efficient in this case than using recursion as the function does not need to keep track of multiple recursive calls on the call stack.
+
+# O(n) time complexity
+# O(1) space complexity
+
+from typing import List
+
+def two_number_sum(seq: List[int], z: int) -> str:
+    """
+    Finds two numbers in the given sequence whose sum equals the target number.
+
+    Parameters:
+    seq (list[int]): The sequence of numbers to search.
+    z (int): The target sum.
+
+    Returns:
+    str: A string containing the values of the two numbers, and a boolean indicating whether their sum equals the target. The values of the two numbers are represented as "x" and "y". If the two numbers are not found, they are represented as "not found".
+    """
+    # initializing the high and low pointers
+    low = 0
+    high = len(seq) - 1
+    # doing a while loop until the pointers meet each other
+    while low < high:
+        curr_sum = seq[low] + seq[high]
+        # If the sum is equal to the target, return the two numbers
+        if curr_sum == z: 
+            return f"x = {seq[low]}\ny = {seq[high]}\n{seq[low] + seq[high] == z}"
+        # If the sum is less than the target, move the low pointer up
+        elif curr_sum < z:
+            low += 1
+        # If the sum is more than the target, move the high pointer down
+        else: 
+            high -= 1
+    # If the pointers meet each other and no sum is found, return not found
+    return f"x = not found\ny = not found\n{False}"
